168c0d36f89e6ed427c1a3a4c324e259189dd117
siva <sivaprakash.s4567@gmail.com>
2026-06-09 18:53:19 +0530

    finish one round game, tested

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 console.log("Hello world");
 
-let human_score, comp_score =0;
+let human_score=0, comp_score =0;
 
 function getComputerChoice(){
     let comp_choice;
@@ -8,27 +8,92 @@ function getComputerChoice(){
     console.log(gen);
     
     if (gen<(1/3)){
-        comp_choice="r";
+        comp_choice="rock";
     }
     else if(gen>(2/3)){
-        comp_choice='p';
+        comp_choice='paper';
     }
     else{
-        comp_choice="s";
+        comp_choice="scissor";
     }
-    console.log(comp_choice);
+    return comp_choice;
+   
 }
 
 function getHumanChoice(){
-
     let human_choice=prompt("Choose your move: Rock, Paper or Scissor?").toLowerCase();
-    console.log(human_choice);
-    
-    
+    return human_choice;
 
 }
 
-getHumanChoice();
-// function playRound(human_choice,comp_choice){
 
-// }
+function playRound(human_choice,comp_choice){
+    console.log(`You choose ${human_choice}! Computer chooses ${comp_choice}!\n`)
+    let victory=0;
+    if(human_choice=="rock"){
+        switch (comp_choice){
+            case "rock":
+                break;
+            case "scissor":
+                human_score+=1;
+                victory=1;
+                break;
+            case "paper":
+                comp_score+=1;
+                victory=-1;
+                break;    
+        }
+    }
+    if(human_choice=="scissor"){
+        switch (comp_choice){
+            case "rock":
+                comp_score+=1;
+                victory=-1;
+                break;
+            case "scissor":
+                break;
+                
+            case "paper":
+                human_score+=1;
+                victory=1;
+                break;
+                
+        }
+    }
+
+    if(human_choice=="paper"){
+        switch (comp_choice){
+            case "rock":
+                human_score+=1
+                victory=1;
+                break;
+            case "scissor":
+                comp_score+=1;
+                victory=-1;
+                break;
+            case "paper":
+                break;
+        }
+    }
+    switch(victory){
+        case 1:
+            console.log("You have won!");
+            break;
+        case -1:
+            console.log("You lost.");
+            break;
+        default:
+            console.log("It is a draw.");
+            break;    
+    }
+    
+
+    console.log(`human score is ${human_score}\ncomputer score is ${comp_score}`);
+
+    
+}
+
+const humanSelection= getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
